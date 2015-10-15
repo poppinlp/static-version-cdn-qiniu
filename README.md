ee2f49a0d2cfb7901cd22550e2253cc492ec6f82
PoppinL <poppinlp@gmail.com>
2015-10-15 12:59:19 +0800

    0.0.2

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # static-version-cdn-qiniu
 
+[![Build Status](https://travis-ci.org/poppinlp/static-version-cdn-qiniu.png?branch=master)](https://travis-ci.org/poppinlp/static-version-cdn-qiniu)
+[![Dependency Status](https://david-dm.org/poppinlp/static-version-cdn-qiniu.svg)](https://david-dm.org/poppinlp/static-version-cdn-qiniu)
+[![devDependency Status](https://david-dm.org/poppinlp/static-version-cdn-qiniu/dev-status.svg)](https://david-dm.org/poppinlp/static-version-cdn-qiniu#info=devDependencies)
+
 Qiniu CDN adapter for static-version package.
+
+## History
+
+- 0.0.2 init
--- adapter.js
+++ adapter.js
@@ -1,6 +1,7 @@
 module.exports = function (config) {
     var qiniu = require('qiniu'),
         bucket = config.bucket,
+        prefix = config.prefix,
         ak = config.ak,
         sk = config.sk;
 
@@ -11,7 +12,7 @@ module.exports = function (config) {
         var token = (new qiniu.rs.PutPolicy(bucket)).token(),
             extra = new qiniu.io.PutExtra();
 
-        qiniu.io.putFile(token, localFile, localFile, extra, function (err, ret) {
+        qiniu.io.putFile(token, prefix + localFile, localFile, extra, function (err, ret) {
             err ? cb(ret) : cb('');
         });
     };
